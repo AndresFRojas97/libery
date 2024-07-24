0eafbe9a58f53117ad976ec34e7a8c355b0ec557
Inge. Felipe <inge.andresfrojas@gmail.com>
2024-07-23 20:27:58 -0500

    Agregar controlador Book

---
+++ Library_API/Controllers/BoolController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoolController : Controller
+    {
+
+        //1. que este metodo retorne una lista de objetos mockeados
+
+        [HttpGet]
+        [Route("GetAllBooks")]
+        public string ReturnTest(int numero)
+        {
+            var result = ($"Has Ingresado el numero: {numero * 5}");
+
+            return result;
+
+        }
+
+        //crear un metodo llamado addBook y otro DeleteBook
+        //investigar sobre LINQ y DTOs
+        // hacer un commit y subir cambios hechos 
+        
+    }
+}
